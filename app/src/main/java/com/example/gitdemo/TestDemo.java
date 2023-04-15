8c763197430cdad7fd9a624b70ac4d4031da64a2
dhruv151104 <85018528+dhruv151104@users.noreply.github.com>
2023-04-15 23:14:55 +0530

    updated testdemo in dhruv branch

--- app/src/main/java/com/example/gitdemo/TestDemo.java
+++ app/src/main/java/com/example/gitdemo/TestDemo.java
@@ -4,5 +4,6 @@ public class TestDemo {
     public static void main(String[] args) {
         System.out.println("Test app");
         System.out.println("Save from dhruv branch");
+        System.out.println("Updated done in same line in dhruv branch for conflict scenario");
     }
 }
